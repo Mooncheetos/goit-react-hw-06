3e12e32f01428a9546fa2cf8de691d9337a08010
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-04-22 00:05:38 +0300

    firConto

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,20 @@
 import css from "./Contact.module.css";
 import { BsFillPersonFill } from "react-icons/bs";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
 
-function Contact ({ data: {id, name, number}, onDelete}) {
+function Contact({ contacts: { id, name, number } }) {
+    
+    const dispatch = useDispatch();
+    const onDeleteContact = (contactId) => { dispatch(deleteContact(contactId)) };
     return (
         <>
         <div>
             <p className={css.contactName}><BsFillPersonFill />{name}</p>
             <p className={css.contactNumber}><BsFillTelephoneFill />{number}</p>
         </div>
-            <button className={css.contactBtn} onClick={() => onDelete(id)}>Delete</button>
+            <button className={css.contactBtn} onClick={() => onDeleteContact(id)}>Delete</button>
             </>
     );
 }
